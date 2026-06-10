820c4e511a6a88adf0d74900cbccaec8329a8058
Johan Schaeffer <johan.schaeffer6@etu.univ-lorraine.fr>
2026-06-10 10:24:43 +0200

    [MERGE] futur merge

---
+++ minipress.core/src/webui/actions/GetHomeAction.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace minipress\webui\actions;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+class GetHomeAction
+{
+      public function __invoke(Request $request, Response $response, array $args): Response
+      {
+            $view = Twig::fromRequest($request);
+
+            return $view->render($response, 'homeView.twig');
+      }
+}
