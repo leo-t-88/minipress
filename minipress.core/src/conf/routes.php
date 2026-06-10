d31492526e7a72c539a825993784d1a377c770ca
Loic Durand <loic.durand7@etu.univ-lorraine.fr>
2026-06-10 11:38:37 +0200

    [ADD] AbstractAction, GetArticleFormAction et PostArticleAction et des routes

--- minipress.core/src/conf/routes.php
+++ minipress.core/src/conf/routes.php
@@ -9,9 +9,13 @@ Eloquent::init(__DIR__ . '/minipress.db.conf.ini');
 
 use \Slim\App;
 use mp\webui\actions\GetHomeAction;
+use mp\webui\actions\GetArticleFormAction;
+use mp\webui\actions\PostArticleAction;
 
 return function (App $app): App {
     $app->get('/', GetHomeAction::class)->setName('home');
+    $app->get('/', GetArticleFormAction::class)->setName('form_article');
+    $app->post('/', PostArticleAction::class)->setName('post_article');
 
     return $app;
 };
