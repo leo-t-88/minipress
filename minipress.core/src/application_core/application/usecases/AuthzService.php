4cc40915188165fedd0da131b7e709b47892199e
leo-t-88 <leo88thomas@gmail.com>
2026-06-10 11:59:43 +0200

    [Add] Service de vérification de permissions

---
+++ minipress.core/src/application_core/application/usecases/AuthzService.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+namespace gift\core\application\usecases;
+
+use Illuminate\Database\QueryException;
+use gift\core\application\exceptions\DataErrorException;
+use mp\core\domain\entities\User;
+use mp\core\domain\entities\Article;
+
+class AuthzService implements AuthzInterface
+{
+    public function isGranted(string $user_id, string $operation, string $article_id): bool
+    {
+        try {
+            $user = User::where('id', $user_id)->first();
+            switch ($operation) {
+                case self::CREATE_ARTICLE:
+                case self::CREATE_CATEGORY:
+                    return $user['role'] >= 1;
+                case self::REGISTER_USER:
+                    return $user['role'] === 100;
+                case self::VIEW_ARTICLE:
+                    $article = Article::findOrFail($article_id);
+                    if ($user['role'] === 100) return true; // ADMIN
+                    return $user['id'] === $article['auteur_id'];
+
+            }
+        } catch (QueryException $e) {
+            throw new DataErrorException('Pas d\'article avec cette id');
+        }
+        return false;
+    }
+}
