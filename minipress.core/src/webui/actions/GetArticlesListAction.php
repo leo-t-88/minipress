4b085b5cf200e2843fa895428b765601e3128806
Loic Durand <loic.durand7@etu.univ-lorraine.fr>
2026-06-11 11:35:20 +0200

    [ADD] filtre de catégories dans la liste des articles

--- minipress.core/src/webui/actions/GetArticlesListAction.php
+++ minipress.core/src/webui/actions/GetArticlesListAction.php
@@ -7,17 +7,30 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 use mp\core\domain\entities\Article;
+use mp\core\domain\entities\Categorie;
 
 class GetArticlesListAction extends AbstractAction
 {
     public function __invoke(Request $request, Response $response, array $args): Response
     {
-        $articles = Article::orderBy('date_creation', 'DESC')->get();
+        $queryParams = $request->getQueryParams();
+        $categorie_id = filter_var($queryParams['categorie_id'] ?? null, FILTER_VALIDATE_INT);
+
+        $query = Article::orderBy('date_creation', 'DESC');
+
+        if ($categorie_id) {
+            $query->where('categorie_id', $categorie_id);
+        }
+
+        $articles = $query->get();
+        $categories = Categorie::all();
 
         $view = Twig::fromRequest($request);
         
         return $view->render($response, 'list_articles.twig', [
-            'articles' => $articles
+            'articles' => $articles,
+            'categories' => $categories,
+            'current_categorie_id' => $categorie_id
         ]);
     }
 }
