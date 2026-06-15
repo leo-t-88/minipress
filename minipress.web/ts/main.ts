40255b782604893f8253198ed37ff454ca318425
leo-t-88 <leo88thomas@gmail.com>
2026-06-15 15:20:37 +0200

    [Fix] Utilisation de type au lieu de any (jspas pk mais ça avait pas commit)

--- minipress.web/ts/main.ts
+++ minipress.web/ts/main.ts
@@ -1,15 +1,19 @@
 import { getArticles } from "./api";
 import { renderArticles } from "./view";
+import { ArticleList } from "./types";
 
 async function init(): Promise<void> {
     const zone = document.getElementById("articles");
 
     try {
-        const response = await getArticles();
+        const response: ArticleList = await getArticles();
 
-        const sortedArticles = response.articles.sort((a: any, b: any) => {
-            return new Date(b.article.date_creation).getTime() - new Date(a.article.date_creation).getTime();
-        });
+        const sortedArticles = response.articles.sort(
+            (a: ArticleList["articles"][number], b: ArticleList["articles"][number]) =>
+                new Date(b.article.date_creation).getTime() -
+                new Date(a.article.date_creation).getTime()
+        );
+
         renderArticles(sortedArticles);
     } catch (error) {
         console.error(error);
