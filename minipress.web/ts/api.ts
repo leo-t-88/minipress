1186617ee65ba452c2d060a86a99794cb7140126
Johan Schaeffer <johan.schaeffer6@etu.univ-lorraine.fr>
2026-06-15 15:32:11 +0200

    [ADD] Fonction de de recuperation d'article via categorie via api

--- minipress.web/ts/api.ts
+++ minipress.web/ts/api.ts
@@ -9,3 +9,11 @@ export async function getArticles() {
 
     return response.json();
 }
+
+export async function getArticlesByCategorie(id: string): Promise<any> {
+  const response = await fetch(`${API_BASE_URL}/categories/${id}/articles`);
+  if (!response.ok) {
+    throw new Error("Erreur lors du chargement des articles");
+  }
+  return await response.json();
+}
