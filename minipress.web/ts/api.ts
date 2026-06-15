3f38e0a4afc1d9dd3eb92b8ca676dcb69cc09651
Johan Schaeffer <johan.schaeffer6@etu.univ-lorraine.fr>
2026-06-15 14:52:17 +0200

    [EDIT] Url base docketu

--- minipress.web/ts/api.ts
+++ minipress.web/ts/api.ts
@@ -1,7 +1,7 @@
-import { API_BASE_URL_TEST } from "./config";
+import { API_BASE_URL } from "./config";
 
 export async function getArticles() {
-    const response = await fetch(`${API_BASE_URL_TEST}/articles`);
+    const response = await fetch(`${API_BASE_URL}/articles`);
 
     if (!response.ok) {
         throw new Error("Erreur lors du chargement des articles");
